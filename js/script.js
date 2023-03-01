0eb9904a775fcd0c754453db0a46d621966eaec9
EslamElsebaey <eslamelsebaey.11@gmail.com>
2023-03-01 16:04:57 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
      // preloader
+     setTimeout(function () {
       $(".preloader").css({
         opacity : "0" , 
         visibility : "hidden" , 
       })
-    
+    }, 2500);
 
 
 
